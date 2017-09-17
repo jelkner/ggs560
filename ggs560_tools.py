0c8ac8a2a3223880547d22eeb9e4a2a3cc66041e
Jeffrey Elkner <jeff@elkner.net>
2017-09-17 13:01:56 -0400

    Add .gitignore, doctests, and sample vs pop arg

--- ggs560_tools.py
+++ ggs560_tools.py
@@ -1,17 +1,37 @@
 def mean(dataset):
+    """
+      >>> mean([1, 2, 3])
+      2.0
+      >>> from sample_data import data_set1 as ds
+      >>> mean(ds)
+      5.58
+    """
     return sum(dataset) / len(dataset)
 
 
-def variance(dataset):
+def variance(dataset, population=False):
+    """
+      >>> from sample_data import data_set1 as ds
+      >>> round(variance(ds), 4)
+      0.4086
+    """
+    div = len(dataset) if population else len(dataset) - 1
     dsmean = mean(dataset)
-    return sum([(xi - dsmean) ** 2 for xi in dataset]) / len(dataset)
+    return sum([(xi - dsmean) ** 2 for xi in dataset]) / div 
 
 
-def std_deviation(dataset):
+def std_deviation(dataset, population=False):
+    """
+      >>> from sample_data import data_set1 as ds
+      >>> round(std_deviation(ds), 4)
+      0.6392
+    """
     n = len(dataset)
+    div = n if population else n - 1
     dsmean = mean(dataset)
-    return ((sum([xi ** 2 for xi in dataset]) - n * dsmean ** 2) / n) ** 0.5
+    return ((sum([xi ** 2 for xi in dataset]) - n * dsmean ** 2) / div) ** 0.5
 
-print(mean(ds))
-print(variance(ds))
-print(std_deviation(ds))
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
