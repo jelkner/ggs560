65ddaaae08964c8a2bb68dc7093dc2180dcedb39
Jeffrey Elkner <jeff@elkner.net>
2017-09-17 12:33:41 -0400

    Add sample data file.

--- ggs560_tools.py
+++ ggs560_tools.py
@@ -12,8 +12,6 @@ def std_deviation(dataset):
     dsmean = mean(dataset)
     return ((sum([xi ** 2 for xi in dataset]) - n * dsmean ** 2) / n) ** 0.5
 
-ds = [19, 13, 8, 14, 12, 15, 15, 12, 16, 15, 28, 10, 15, 16]
-
 print(mean(ds))
 print(variance(ds))
 print(std_deviation(ds))
